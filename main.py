2f1e775b27c8d299c685765374686cf501fcf714
Jungwoo Hong <redfrienz@gmail.com>
2022-05-24 12:19:19 +0900

    점프 유사 구현

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ pygame.init()
 
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
+SIZE=90
+velocity=0
 white = (255,255,255)
 black = (0,0,0)
 #asdf
@@ -18,28 +20,38 @@ clock = pygame.time.Clock()
 def display_player():
     screen.fill(black)
     #pygame.draw.circle(screen,white,(playerXpos,playerYpos),100)
-    pygame.draw.rect(screen, white, (playerXpos, playerYpos, 90, 90))
+    pygame.draw.rect(screen, white, (playerXpos, playerYpos, SIZE, SIZE))
     pygame.draw.rect(screen, white, (0, 1030, 1920, 50))
 
 def movebykey(speed):
-    global playerXpos, playerYpos
+    global playerXpos, playerYpos, velocity
     key_event = pygame.key.get_pressed()
     if key_event[pygame.K_LEFT]:
         playerXpos -= speed
     if key_event[pygame.K_RIGHT]:
         playerXpos += speed
-    if key_event[pygame.K_UP]:
-        playerYpos -= speed
-    if key_event[pygame.K_DOWN]:
-        playerYpos += speed
 
     display_player()
-    stayinside(speed)
+    vel()
+    movebyvelocity(velocity)
+    stayinside()
 
-def stayinside(speed):
-    global playerXpos, playerYpos
+def vel():
+    global playerXpos, playerYpos, velocity
+    key_event = pygame.key.get_pressed()
+    if key_event[pygame.K_UP]:
+         velocity += 5
+    velocity -= 1
+
+def movebyvelocity(v):
+    global playerYpos
+    playerYpos -= v
+
+def stayinside():
+    global playerXpos, playerYpos, velocity
     if playerYpos >= 940:
-        playerYpos -= speed
+        playerYpos -= velocity
+        velocity = 0
 
 while True:
     clock.tick(60)
